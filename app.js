7c85255b0ee4ce9a274dc7d05a1e130e69b32163
Adam Romero <adamromero89@gmail.com>
2020-01-22 00:18:53 -0800

    Optimize webp images and render iframe on click

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var expressLayouts = require('express-ejs-layouts');
-var index = require('./routes/index');
-var connection  = require('express-myconnection');
-var session = require('express-session');
-var mysql = require('mysql');
-var keys = require('./config');
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var expressLayouts = require("express-ejs-layouts");
+var index = require("./routes/index");
+var connection = require("express-myconnection");
+var session = require("express-session");
+var mysql = require("mysql");
+var keys = require("./config");
 const cacheTime = 86400000 * 30;
 var app = express();
 
 var dbOptions = {
-	host     : keys.DB_HOST,
-	user     : keys.DB_USER,
-	password : keys.DB_PASSWORD,
-	database : keys.DB_DATABASE
-}
+   host: keys.DB_HOST,
+   user: keys.DB_USER,
+   password: keys.DB_PASSWORD,
+   database: keys.DB_DATABASE
+};
+
+app.use(connection(mysql, dbOptions, "pool"));
 
 app.use(
-    connection(mysql, dbOptions,'pool')
+   session({
+      secret: "secret",
+      resave: true,
+      saveUninitialized: true
+   })
 );
 
-app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
-}));
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressLayouts);
-app.use(express.static(path.join(__dirname, 'dist'), {
-	maxAge: cacheTime
-}));
-app.use('/', index);
+app.use(
+   express.static(path.join(__dirname, "dist"), {
+      maxAge: cacheTime
+   })
+);
+app.use("/", index);
 
 app.listen(process.env.PORT || keys.PORT);
 
--- src/app.js
+++ src/app.js
@@ -1,51 +1,52 @@
-require('./stylesheets/style.scss');
+require("./stylesheets/style.scss");
 
 var app = (function() {
-	var s;
-	var app = {
-		settings: {
-			mobileBreakPoint: 642,
-			wrapper: document.querySelector('.wrapper'),
-			mobileMenu: document.querySelector('.mobile-navigation-menu'),
-			contentItem: document.querySelectorAll(".content-item"),
-			modalWindow: document.getElementById('modalWindow'),
-			closeModalWindow: document.querySelector('.modal-close'),
-			modalContent: document.querySelector('.modal-content'),
-			menuIcon: document.querySelector(".menu-icon"),
-			inputSearch: document.querySelector(".input-search"),
-			gridColumn: document.querySelectorAll(".grid-column")
-			//videoPlayer: document.getElementById("videoPlayer")
-		},
+   var s;
+   var app = {
+      settings: {
+         mobileBreakPoint: 642,
+         wrapper: document.querySelector(".wrapper"),
+         mobileMenu: document.querySelector(".mobile-navigation-menu"),
+         contentItem: document.querySelectorAll(".content-item"),
+         modalWindow: document.getElementById("modalWindow"),
+         closeModalWindow: document.querySelector(".modal-close"),
+         modalContent: document.querySelector(".modal-content"),
+         menuIcon: document.querySelector(".menu-icon"),
+         inputSearch: document.querySelector(".input-search"),
+         gridColumn: document.querySelectorAll(".grid-column")
+         //videoPlayer: document.getElementById("videoPlayer")
+      },
 
-		init: function() {
-			s = this.settings;
-			this.openMenuForDesktop();
-			this.bindUIActions();
-		},
+      init: function() {
+         s = this.settings;
+         this.openMenuForDesktop();
+         this.lazyLoad();
+         this.bindUIActions();
+      },
 
-		bindUIActions: function() {
-			window.addEventListener('resize', app.openMenuForDesktop, false);
+      bindUIActions: function() {
+         window.addEventListener("resize", app.openMenuForDesktop, false);
 
-			s.menuIcon.addEventListener('click', function(e) {
-				[].map.call(document.querySelectorAll(".menu-icon"), function(el) {
-			        el.classList.toggle('open');
-			        s.mobileMenu.classList.toggle('is-open');
-			        s.wrapper.classList.toggle('is-shifted');
-			    });
-			});
+         s.menuIcon.addEventListener("click", function(e) {
+            [].map.call(document.querySelectorAll(".menu-icon"), function(el) {
+               el.classList.toggle("open");
+               s.mobileMenu.classList.toggle("is-open");
+               s.wrapper.classList.toggle("is-shifted");
+            });
+         });
 
-			app.clickToOpenContent(s.contentItem, '.content-item__source', false);
+         app.clickToOpenContent(s.contentItem, ".content-item__source", false);
 
-			s.closeModalWindow.addEventListener('click', function(e) {
-				app.closeModal();
-			});
+         s.closeModalWindow.addEventListener("click", function(e) {
+            app.closeModal();
+         });
 
-			if (s.inputSearch !== null && s.inputSearch !== "undefined") {
-				s.inputSearch.addEventListener('keyup', function() {
-					app.filterItemsBySearch(this);
-				});
-			}
-			/*
+         if (s.inputSearch !== null && s.inputSearch !== "undefined") {
+            s.inputSearch.addEventListener("keyup", function() {
+               app.filterItemsBySearch(this);
+            });
+         }
+         /*
 			if (s.videoPlayer) {
 				s.videoPlayer.addEventListener("onStateChange", function(state){
 					console.log(state);
@@ -54,121 +55,153 @@ var app = (function() {
 					}
 				});
 			}*/
-		},
+      },
 
-		filterItemsBySearch: function (input) {
-			let filter = input.value.toUpperCase();
-			let gridColumn = s.gridColumn;
+      lazyLoad: function() {},
 
-			for (let i = 0; i < gridColumn.length; i++) {
-				let gridColumnTitles = gridColumn[i].childNodes[1].innerText;
-				if (gridColumnTitles) {
-					if (gridColumnTitles.toUpperCase().indexOf(filter) > -1) {
-						gridColumn[i].style.display = "";
-					} else {
-						gridColumn[i].style.display = "none";
-					}
-				}
-			}
-		},
+      filterItemsBySearch: function(input) {
+         let filter = input.value.toUpperCase();
+         let gridColumn = s.gridColumn;
 
-		closeModal: function() {
-			s.modalWindow.classList.remove('is-open');
-			s.modalWindow.classList.remove('is-locked');
-			s.modalContent.classList.remove('responsive-video');
-			var content = $(s.modalContent).children('.content-item__source')[0];
-			content.parentNode.removeChild(content);
-			document.body.classList.remove('is-locked');
-			responsiveVoice.cancel();
-		},
+         for (let i = 0; i < gridColumn.length; i++) {
+            let gridColumnTitles = gridColumn[i].childNodes[1].innerText;
+            if (gridColumnTitles) {
+               if (gridColumnTitles.toUpperCase().indexOf(filter) > -1) {
+                  gridColumn[i].style.display = "";
+               } else {
+                  gridColumn[i].style.display = "none";
+               }
+            }
+         }
+      },
 
-		clickToOpenContent: function(contents, selector) {
-			contents.forEach(function(content) {
-				content.addEventListener("click", function() {
-					let sourceContent = $(this).children(selector)[0].cloneNode(true);
-					let isVideo = sourceContent.tagName === 'IFRAME';
-					app.insertContentInModal(sourceContent, isVideo);
+      closeModal: function() {
+         s.modalWindow.classList.remove("is-open");
+         s.modalWindow.classList.remove("is-locked");
+         s.modalContent.classList.remove("responsive-video");
+         var content = $(s.modalContent).children(".content-item__source")[0];
+         content.parentNode.removeChild(content);
+         document.body.classList.remove("is-locked");
+         responsiveVoice.cancel();
+      },
 
-					if (!isVideo) {
-						app.arrangePhotosWithinText();
-					}
-				});
-			});
+      clickToOpenContent: function(contents, selector) {
+         contents.forEach(function(content) {
+            content.addEventListener("click", function() {
+               let sourceContent,
+                  isVideo = false;
+               if (this.classList.contains("video-content")) {
+                  sourceContent = $(this).find(".content-item__video");
+                  isVideo = true;
+               } else {
+                  sourceContent = $(this)
+                     .children(selector)[0]
+                     .cloneNode(true);
+               }
+               app.insertContentInModal(sourceContent, isVideo);
+               if (!isVideo) {
+                  app.arrangePhotosWithinText();
+               }
+            });
+         });
 
-			document.querySelectorAll(".text-content__story").forEach(function(story) {
-				story.innerHTML = story.innerText;
-			});
-		},
+         document
+            .querySelectorAll(".text-content__story")
+            .forEach(function(story) {
+               story.innerHTML = story.innerText;
+            });
+      },
 
-		insertAfter(newNode, referenceNode) {
-			referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-		},
+      insertAfter(newNode, referenceNode) {
+         referenceNode.parentNode.insertBefore(
+            newNode,
+            referenceNode.nextSibling
+         );
+      },
 
-		arrangePhotosWithinText: function() {
-			let storyParagraphs = document.querySelectorAll(".is-open .text-content__story p");
-			let storyImages = document.querySelectorAll(".is-open .story-images img");
+      arrangePhotosWithinText: function() {
+         let storyParagraphs = document.querySelectorAll(
+            ".is-open .text-content__story p"
+         );
+         let storyImages = document.querySelectorAll(
+            ".is-open .story-images img"
+         );
 
-			let storyLength = storyParagraphs.length;
-			let numImages = storyImages.length;
-			let target = parseInt(storyLength / numImages);
+         let storyLength = storyParagraphs.length;
+         let numImages = storyImages.length;
+         let target = parseInt(storyLength / numImages);
 
-			let imageIdx = 0;
-			for (let i = 0; i < storyLength; i++) {
-				if (i % target === 0 && imageIdx < numImages) {
-					app.insertAfter(storyImages[imageIdx++], storyParagraphs[i]);
-				}
-			}
-		},
+         let imageIdx = 0;
+         for (let i = 0; i < storyLength; i++) {
+            if (i % target === 0 && imageIdx < numImages) {
+               app.insertAfter(storyImages[imageIdx++], storyParagraphs[i]);
+            }
+         }
+      },
 
-		openMenuForDesktop: function() {
-			if (window.innerWidth >= s.mobileBreakPoint) {
-				if (!app.hasClass(s.mobileMenu.classList, 'is-open')) {
-					s.mobileMenu.classList.add('is-open');
-				}
-				if (!app.hasClass(s.wrapper.classList, 'is-shifted')) {
-					s.wrapper.classList.add('is-shifted');
-				}
-			} else {
-				if (app.hasClass(s.mobileMenu.classList, 'is-open')) {
-					s.mobileMenu.classList.remove('is-open');
-				}
-				if (app.hasClass(s.wrapper.classList, 'is-shifted')) {
-					s.wrapper.classList.remove('is-shifted');
-				}
-			}
-			s.menuIcon.classList.remove('open');
-		},
+      openMenuForDesktop: function() {
+         if (window.innerWidth >= s.mobileBreakPoint) {
+            if (!app.hasClass(s.mobileMenu.classList, "is-open")) {
+               s.mobileMenu.classList.add("is-open");
+            }
+            if (!app.hasClass(s.wrapper.classList, "is-shifted")) {
+               s.wrapper.classList.add("is-shifted");
+            }
+         } else {
+            if (app.hasClass(s.mobileMenu.classList, "is-open")) {
+               s.mobileMenu.classList.remove("is-open");
+            }
+            if (app.hasClass(s.wrapper.classList, "is-shifted")) {
+               s.wrapper.classList.remove("is-shifted");
+            }
+         }
+         s.menuIcon.classList.remove("open");
+      },
 
-		hasClass: function(classList, selector) {
-			for (var i = 0; i < classList.length; i++) {
-				if (classList[i] === selector) {
-					return true;
-				}
-			}
-			return false;
-		},
+      hasClass: function(classList, selector) {
+         for (var i = 0; i < classList.length; i++) {
+            if (classList[i] === selector) {
+               return true;
+            }
+         }
+         return false;
+      },
 
-		insertContentInModal: function(content, isVideo) {
-			content.classList.remove('hide');
-			s.modalWindow.classList.add('is-open');
-			s.modalContent.appendChild(content);
-			document.body.classList.add('is-locked');
+      insertContentInModal: function(content, isVideo) {
+         if (isVideo) {
+            var windowHeight =
+               window.innerHeight ||
+               document.documentElement.clientHeight ||
+               document.body.clientHeight;
 
-			if (isVideo) {
-				var windowHeight = window.innerHeight
-					|| document.documentElement.clientHeight
-					|| document.body.clientHeight;
+            var urlVideoID = content[0].getAttribute("data-video");
+            console.log("content id: ", urlVideoID);
 
-				s.modalWindow.classList.add('is-locked');
-				s.modalContent.classList.add('responsive-video');
-				s.modalContent.childNodes[0].style.height = (windowHeight * .75).toString() + "px";
-				content.src += "&autoplay=1";
-				content.setAttribute("allow", "autoplay");
-			}
-		}
-	};
+            var iframe = `<iframe
+               allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+               id="videoPlayer"
+               class="content-item__source iframe-content lazy"
+               src="https://www.youtube.com/embed/${urlVideoID}?iv_load_policy=3&rel=0&enablejsapi=1&widgetid=1&autoplay=1"
+               allowfullscreen style="height: ${(
+                  windowHeight * 0.75
+               ).toString()}px">
+               </iframe>`;
 
-	return app;
+            s.modalWindow.classList.add("is-locked");
+            s.modalContent.classList.add("responsive-video");
+            s.modalContent.innerHTML = iframe;
+            s.modalWindow.classList.add("is-open");
+            document.body.classList.add("is-locked");
+         } else {
+            content.classList.remove("hide");
+            s.modalWindow.classList.add("is-open");
+            s.modalContent.appendChild(content);
+            document.body.classList.add("is-locked");
+         }
+      }
+   };
+
+   return app;
 })();
 
 app.init();
